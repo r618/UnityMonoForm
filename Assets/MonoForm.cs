644ff61bf9fe614471f673bf924522174cfa9116
Martin Cvengros <mcv618@gmail.com>
2022-10-15 15:41:46 +0200

    form show 0

---
+++ Assets/MonoForm.cs
@@ -0,0 +1,19 @@
+﻿public class MonoForm : System.Windows.Forms.Form
+{
+    public MonoForm()
+    {
+        InitializeComponent();
+
+    }
+    void InitializeComponent()
+    {
+        // 
+        // MainForm
+        // 
+        this.AutoScaleBaseSize = new System.Drawing.Size(5, 13);
+        this.ClientSize = new System.Drawing.Size(472, 382);
+
+        this.Name = "MonoForm";
+        this.Text = "MonoForm";
+    }
+}
---
+++ Assets/MonoWindow.cs
@@ -0,0 +1,16 @@
+﻿using System.Windows.Forms;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class MonoWindow : MonoBehaviour
+{
+    Form form;
+    void OnGUI()
+    {
+        if (GUILayout.Button("MonoForm"))
+        {
+            this.form = new MonoForm();
+            this.form.Show();
+        }
+    }
+}
